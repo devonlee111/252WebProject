17d49395e769b328682bf36de46c992e2833318c
devonlee111 <devonlee111@gmail.com>
2017-12-07 00:02:52 +0000

    Added PHP Create Account Script
    Created server side php script to create a new account.

---
+++ webserver/createaccount.php
@@ -0,0 +1,34 @@
+<?php
+	//createaccount.php
+	$_GET = array();
+	foreach($argv as $key => $pair) {
+		if ($key == 0) {
+			continue;
+		}
+		list($key, $value) = explode(":", $pair);
+		$_GET[$key] = $value;
+	}
+	$return = "false";
+	$server = "webserverdatabase.clhpvrxrhwvh.us-east-2.rds.amazonaws.com";
+	$username = "Admin252";
+	$password = "252Database";
+	$database = "WebServer";
+	
+	$user = $_GET["username"];
+	$pass = $_GET["password"];
+	$conn = new mysqli($server, $username, $password, $database);
+		
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	$sql = "SELECT name, password FROM users";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows == 0) {
+		$return = "true";
+		$sql = "INSERT INTO users (name, password) VALUES('" . $user . "','" . $pass "')";
+	}
+	echo $return;
+	$conn->close();
+?>
