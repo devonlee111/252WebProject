4a89d89e5835225a45a1163a5b42754e43d540e8
devonlee111 <devonlee111@gmail.com>
2017-12-07 16:15:55 +0000

    Created Javascript For Sign In
    Created javascript program for signing in. Also edited the sign in and create account html pages so that the logo at the top takes you back to the main page.

---
+++ webserver/files/html/SignIn.js
@@ -0,0 +1,70 @@
+var username;
+var password;
+
+function body_onload() {
+	sessionStorageSet("CurrentUser",  null);
+	signInBtn.onclick = signInBtn_onclick;
+
+	usernameInput.value = localStorageGet("Username", "");
+
+	if (localStorageGet("Remember", false) === "false") {
+		rememberChkBox.checked = false;
+	}
+	else {
+		rememberChkBox.checked = localStorageGet("Remember", false);
+	}
+
+	if (usernameInput.value == "") {
+		usernameInput.focus();
+	}
+	else {
+		passwordInput.focus();
+	}
+}
+
+function signInBtn_onclick() {
+	if (usernameInput.value.length <= 0) {
+		alert("Username is required.");
+		usernameInput.focus();
+	}
+	else if (passwordInput.value.length <= 0) {
+		alert("Password is requred.");
+	}
+	else {
+		username = usernameInput.value;
+		password = passwordInput.value;
+		localStorageSet("Remember", rememberChkBox.checked);
+		if (rememberChkBox.checked === true) {
+			localStorageSet("Username", usernameInput.value);
+		}
+		else {
+			localStorageSet("Username", "");
+		}
+		sessionStorageSet("CurrentUser", usernameInput.value);
+		sessionStorageSet("Password", passwordInput.value);
+		attemptLogin();
+	}
+}
+
+function attemptLogin() {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(this.responseText);
+			var response = this.responseText;
+			checkResponse(response);
+		}
+	};
+	xmlhttp.open("GET", "SignIn.php?username=" + username + "&password=" + password, true);
+	xmlhttp.send();
+}
+
+function checkResponse(response) {
+	if (response === "true") {
+		sessionStorageSet("LoggedIn", null);
+		location.href = "MainPage.html";
+	}
+	if (response === "false") {
+		alert("Incorrect username or password.");
+	}
+}
